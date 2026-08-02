4202ba34d3dbe5ffbc61512a7bb6b617612a341d
Dave <david.tomusk@gmail.com>
2026-08-02 13:45:23 +0100

    Create entities

---
+++ Server/Domain/Entities/Member.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities;
+
+public class Member
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+
+    public Member(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+}
---
+++ Server/Domain/Entities/Pairing.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain.Entities;
+
+public class Pairing
+{
+    public Guid Id { get; init; }
+    public Guid FirstMemberId { get; init; }
+    public Guid SecondMemberId { get; init; }
+    public bool IsExempted { get; private set; }
+    public decimal CompatibilityRating { get; private set; }
+    public int NumberOfRatings { get; private set; }
+
+    public Pairing(Guid firstMemberId, Guid secondMemberId)
+    {
+        Id = Guid.NewGuid();
+        FirstMemberId = firstMemberId;
+        SecondMemberId = secondMemberId;
+        IsExempted = false;
+        CompatibilityRating = 0.0m;
+        NumberOfRatings = 0;
+    }
+
+    public void Rate(decimal rating)
+    {
+        NumberOfRatings++;
+        CompatibilityRating = ((CompatibilityRating * (NumberOfRatings - 1)) + rating) / NumberOfRatings;
+    }
+
+    // Real couples are exempted from being rated
+    public void SetExempted()
+    {
+        IsExempted = true;
+    }
+}
