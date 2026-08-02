a3e145f954c08534be061336179481bf230f2ba2
Dave <david.tomusk@gmail.com>
2026-08-02 14:46:20 +0100

    Add infrastructure layer and setup docker compose with postgres and pgadmin

---
+++ Server/Infrastructure/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Pairing> Pairings { get; set; }
+}
