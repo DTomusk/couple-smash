83db01ce56e0bf0eb0e29bcc4eca97935af2d549
Dave <david.tomusk@gmail.com>
2026-08-03 21:10:22 +0100

    Add query client provider and env example

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,20 @@ import App from './App.tsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import { appTheme } from './theme.ts'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { initApiClient } from './lib/client.ts'
+
+initApiClient(import.meta.env.VITE_API_BASE_URL);
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={appTheme}>
       <CssBaseline />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 )
