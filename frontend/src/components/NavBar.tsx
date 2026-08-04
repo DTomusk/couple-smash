a4bf167294e161d2add8f320f47acbac87051af5
Dave <david.tomusk@gmail.com>
2026-08-04 13:49:00 +0100

    Add link to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,18 @@
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
+    const navigate = useNavigate();
     return (
         <>
             <AppBar position="fixed" color="primary">
                 <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Typography variant="h5">💍Couple Smash💑</Typography>
+                    <Button variant="text" color="inherit" onClick={() => navigate('/optimal-pairings')}>
+                        Optimal Pairings
+                    </Button>
                 </Toolbar>
             </AppBar>
         </>
