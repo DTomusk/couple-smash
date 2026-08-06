9dd0456df60e0d994e44616658d5238c24f6ebd4
Dave <david.tomusk@gmail.com>
2026-08-06 22:31:09 +0100

    End the project

--- frontend/src/app/AppRoutes.tsx
+++ frontend/src/app/AppRoutes.tsx
@@ -2,20 +2,26 @@ import { createBrowserRouter } from 'react-router-dom'
 import AppLayout from '../layout/AppLayout'
 import HomePage from '../pages/HomePage'
 import OptimalPairingsPage from '../pages/OptimalPairingsPage'
+import GonePage from '../pages/GonePage'
     
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
         children: [
+            // {
+            //     index: true,
+            //     element: <HomePage />
+            // },
+            // {
+            //     path: 'optimal-pairings',
+            //     element: <OptimalPairingsPage />
+            // },
             {
                 index: true,
-                element: <HomePage />
-            },
-            {
-                path: 'optimal-pairings',
-                element: <OptimalPairingsPage />
-            },
+                path: '*',
+                element: <GonePage />
+            }
         ]
     },
 ])
---
+++ frontend/src/pages/GonePage.tsx
@@ -0,0 +1,8 @@
+export default function GonePage() {
+    return (
+        <Stack spacing={2} sx={{ padding: 1, overflowY: 'auto' }}>
+            <Typography variant="h1">Gone</Typography>
+            <Typography variant="body1">CoupleSmash is gone for good. Thank you for coming along for the ride.</Typography>
+        </Stack>
+    )
+}
