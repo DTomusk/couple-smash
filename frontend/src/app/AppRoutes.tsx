2441d3804385dfa2f522bf0ccf2c63a9faf31652
Dave <david.tomusk@gmail.com>
2026-08-06 22:34:11 +0100

    Fix build

--- frontend/src/app/AppRoutes.tsx
+++ frontend/src/app/AppRoutes.tsx
@@ -1,7 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
 import AppLayout from '../layout/AppLayout'
-import HomePage from '../pages/HomePage'
-import OptimalPairingsPage from '../pages/OptimalPairingsPage'
 import GonePage from '../pages/GonePage'
     
 export const router = createBrowserRouter([
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,10 +1,8 @@
-import { Button, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
-    const navigate = useNavigate();
     return (
         <>
             <AppBar position="fixed" color="primary">
